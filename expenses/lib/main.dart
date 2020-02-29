f206712da12322eb0b0bcb8a8fd7050f931d1c72
Joao <dp.joaopedro@gmail.com>
2020-02-29 14:07:36 -0300

    Buttons

--- expenses/lib/main.dart
+++ expenses/lib/main.dart
@@ -16,23 +16,32 @@ class MyHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text("Despesas Pessoais"),
-        ),
-        body: SingleChildScrollView(
-                  child: Column(
-              crossAxisAlignment: CrossAxisAlignment.stretch,
-              children: <Widget>[
-                Container(
-                  child: Card(
-          color: Colors.blue,
-          child: Text('Gráfico'),
-          elevation: 5
-                  ),
-                ),
-                TransactionUser(),
-              ],
+      appBar: AppBar(
+        title: Text("Despesas Pessoais"),
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.add),
+            onPressed: () {},
+          )
+        ],
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            Container(
+              child: Card(
+                  color: Colors.blue, child: Text('Gráfico'), elevation: 5),
             ),
-        ));
+            TransactionUser(),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        onPressed: null,
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+    );
   }
 }
