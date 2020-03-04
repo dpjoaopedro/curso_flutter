0ad8d45e97ff7ffde6982161101be3b983e1ca79
F05249D <F05249D@SBRNL1DT0082028>
2020-03-04 08:47:01 -0300

    chart

--- expenses/lib/components/Chart.dart
+++ expenses/lib/components/Chart.dart
@@ -1,20 +1,22 @@
+import 'package:expenses/components/chart_bar.dart';
 import 'package:expenses/models/transaction.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 
 class Chart extends StatelessWidget {
-
   final List<Transaction> recentTransaction;
 
   Chart(this.recentTransaction);
 
-  List<Map<String, Object>> get groupedTransaction {
+  List<Map<String, Object>> get _groupedTransaction {
     return List.generate(7, (index) {
-      final weekDay = DateTime.now().subtract(Duration(days: index),);
+      final weekDay = DateTime.now().subtract(
+        Duration(days: index),
+      );
 
       double totalSum = 0.00;
 
-      for(var i = 0; i < recentTransaction.length; i++) {
+      for (var i = 0; i < recentTransaction.length; i++) {
         int diffDays = weekDay.difference(recentTransaction[i].date).inDays;
         bool isSame = (diffDays == 0);
 
@@ -23,11 +25,13 @@ class Chart extends StatelessWidget {
         }
       }
 
-      return {
-        'day': DateFormat.E().format(weekDay)[0],
-        'value': totalSum
-      };
+      return {'day': DateFormat.E().format(weekDay)[0], 'value': totalSum};
+    });
+  }
 
+  double get _weekTotalValue {
+    return _groupedTransaction.fold(0.0, (sum, tr) {
+      return sum + tr['value'];
     });
   }
 
@@ -36,9 +40,20 @@ class Chart extends StatelessWidget {
     return Card(
       elevation: 6,
       margin: EdgeInsets.all(20),
-      child: Row(children: groupedTransaction.map((tr) {
-        return Text('${tr['day']}: ${tr['value']}');
-          }).toList()
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: _groupedTransaction.reversed.map((tr) {
+          return Flexible(
+            fit: FlexFit.tight,
+                    child: ChartBar(
+              label: tr['day'],
+              value: tr['value'],
+              percentage: (tr['value'] as double) / _weekTotalValue,
+            ),
+          );
+        }).toList()),
       ),
     );
   }
--- expenses/lib/main.dart
+++ expenses/lib/main.dart
@@ -1,4 +1,4 @@
-import 'package:expenses/components/Chart.dart';
+import 'package:expenses/components/chart.dart';
 import 'package:expenses/components/transaction_form.dart';
 import 'package:flutter/material.dart';
 import 'package:expenses/models/transaction.dart';
